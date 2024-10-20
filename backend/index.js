cd1bcd843bf3e084085392d719061527214a696d
advayS10 <163100260+advayS10@users.noreply.github.com>
2024-10-20 14:57:37 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const mongoDB = require('./db')
 mongoDB()
 
 app.use((req,res,next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173", "https://team-up-six.vercel.app/login")
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173" || "https://team-up-six.vercel.app/login")
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
