5bcbce45df3e64e0cb90267d5ee772a6258bdf8e
advayS10 <163100260+advayS10@users.noreply.github.com>
2024-10-20 15:04:04 +0530

    cors error checking

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,33 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoDB = require('./db')
+const cors = require('cors')
 
 mongoDB()
 
 const allowedOrigins = [ "http://localhost:5173" , "https://team-up-six.vercel.app"]
 
-app.use((req,res,next) => {
-  res.setHeader("Access-Control-Allow-Origin", allowedOrigins)
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  )
-  next()
-})
+// app.use((req,res,next) => {
+//   res.setHeader("Access-Control-Allow-Origin", allowedOrigins)
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   )
+//   next()
+// })
 
+app.use(cors({
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,  // Allows cookies from frontend
+}));
 
 app.use(express.json())
 app.use('/api', require('./routes/CreateUser'))
